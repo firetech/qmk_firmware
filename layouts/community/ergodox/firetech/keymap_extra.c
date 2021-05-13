41d1732b8ddfbe88c120b42868e6e3db9e0257de
Joakim Tufvegren <jocke@barbanet.com>
2021-05-14 01:54:39 +0200

    Update my keymap for split_common on Ergodox Infinity.

--- layouts/community/ergodox/firetech/keymap_extra.c
+++ layouts/community/ergodox/firetech/keymap_extra.c
@@ -14,89 +14,89 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include QMK_KEYBOARD_H
 #include "wpm.h"
 #include "keymap_extra.h"
 #include <stdio.h>
 
-#ifdef SERIAL_LINK_ENABLE
-#    include "serial_link/protocol/transport.h"
-#    include "serial_link/system/serial_link.h"
-#endif
-
 #define WPM_BUF_SIZE 4
 
 enum custom_keycodes {
-  WPM_RESET = SAFE_RANGE,
-  WPM_MAX,
-  WPM_CURR
+    WPM_RESET = SAFE_RANGE,
+    WPM_MAX,
+    WPM_CURR
 };
 
-uint8_t max_wpm = 0;
+static uint8_t max_wpm = 0;
 
-#ifdef SERIAL_LINK_ENABLE
-MASTER_TO_ALL_SLAVES_OBJECT(max_wpm, uint8_t);
+#if defined(SPLIT_KEYBOARD) && defined(WPM_ENABLE)
+#    include "transactions.h"
 
-static remote_object_t* remote_objects[] = {
-    REMOTE_OBJECT(max_wpm),
-};
+#    ifndef FORCED_SYNC_THROTTLE_MS
+#        define FORCED_SYNC_THROTTLE_MS 100
+#    endif
 
-void matrix_init_user(void) {
-    add_remote_objects(remote_objects, sizeof(remote_objects) / sizeof(remote_object_t*));
+void max_wpm_slave_handler(uint8_t in_buflen, const void* in_data, uint8_t out_buflen, void* out_data) {
+    set_max_wpm(*((uint8_t *)in_data));
 }
 
-static void send_max_wpm(void) {
-    *begin_write_max_wpm() = max_wpm;
-    end_write_max_wpm();
+static bool max_wpm_changed = false;
+
+void housekeeping_task_user(void) {
+    if (is_keyboard_master()) {
+        static uint16_t last_max_wpm_update = 0;
+        if (max_wpm_changed || timer_elapsed(last_max_wpm_update) > FORCED_SYNC_THROTTLE_MS) {
+            if (transaction_rpc_send(MAX_WPM, sizeof(max_wpm), &max_wpm)) {
+                max_wpm_changed = false;
+                last_max_wpm_update = timer_read();
+            }
+        }
+    }
 }
 #endif
 
+void keyboard_post_init_user(void) {
+#ifdef SPLIT_KEYBOARD
+    transaction_register_rpc(MAX_WPM, max_wpm_slave_handler);
+#endif
+}
+
 void set_max_wpm(uint8_t new_max) {
-  max_wpm = new_max;
-#ifdef SERIAL_LINK_ENABLE
-  send_max_wpm();
+    max_wpm = new_max;
+#ifdef SPLIT_KEYBOARD
+    max_wpm_changed = true;
 #endif
 }
 
 uint8_t get_max_wpm(void) {
-#ifdef SERIAL_LINK_ENABLE
-    if (is_serial_link_connected()) {
-        uint8_t* new_max = read_max_wpm();
-        if (new_max) {
-            max_wpm = *new_max;
-        }
-    }
-#endif
-  return max_wpm;
+    return max_wpm;
 }
 
 __attribute__((weak)) void set_hand_swap(bool do_swap) { }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  uint8_t wpm = get_current_wpm();
-  if (wpm > max_wpm) {
-    max_wpm = wpm;
-#ifdef SERIAL_LINK_ENABLE
-    send_max_wpm();
-#endif
-  }
-  switch (keycode) {
-    case WPM_RESET:
-      if (record->event.pressed) {
-        set_max_wpm(0);
-        set_current_wpm(0);
-      }
-      break;
-    case WPM_MAX:
-    case WPM_CURR:
-      if (record->event.pressed) {
-        char wpm_buf[WPM_BUF_SIZE];
-        snprintf(wpm_buf, WPM_BUF_SIZE, "%u", (keycode == WPM_MAX ? get_max_wpm() : wpm));
-        send_string(wpm_buf);
-      }
-      break;
-    case SH_T(KC_SPC):
-      set_hand_swap(record->event.pressed);
-      break;
-  }
-  return true;
+    uint8_t wpm = get_current_wpm();
+    if (wpm > max_wpm) {
+        set_max_wpm(wpm);
+    }
+    switch (keycode) {
+        case WPM_RESET:
+            if (record->event.pressed) {
+                set_max_wpm(0);
+                set_current_wpm(0);
+            }
+            break;
+        case WPM_MAX:
+        case WPM_CURR:
+            if (record->event.pressed) {
+                char wpm_buf[WPM_BUF_SIZE];
+                snprintf(wpm_buf, WPM_BUF_SIZE, "%u", (keycode == WPM_MAX ? get_max_wpm() : wpm));
+                send_string(wpm_buf);
+            }
+            break;
+        case SH_T(KC_SPC):
+            set_hand_swap(record->event.pressed);
+            break;
+    }
+    return true;
 };
